90edd395eabfc2bcd41f38399163ff64ae65a3b8
Fuck OFF! <imchale@globexindustries.org>
2026-02-20 21:12:36 -0500

    Initial Commit

---
+++ Scorpio's Flamethrower/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scorpio's Flamethrower
+//
+//  Created by Ian McHale on 2/20/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
